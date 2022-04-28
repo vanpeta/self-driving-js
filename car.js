282918f09bd32abcc2c55517a309849cb08a2949
Carlos Allende <carlosallende@carloss-imac.attlocal.net>
2022-04-28 10:30:26 -0700

    implementing colission detection

--- car.js
+++ car.js
@@ -11,6 +11,8 @@ class Car {
         this.imageForward.src = './car-forward-image.png';
         this.imageBackward = new Image(this.width, this.height);
         this.imageBackward.src = './car-backward-image.png';
+        this.imageDamage = new Image(this.width, this.height);
+        this.imageDamage.src = './car-damaged.png';
 
         // initialize image
         this.image = this.stillImage;
@@ -19,7 +21,8 @@ class Car {
         this.acceleration = 0.2;
         this.maxSpeed = 3;
         this.friction = 0.05;
-        this.angle=0;
+        this.angle = 0;
+        this.damaged = false;
 
         this.sensor = new Sensor(this);
 
@@ -27,8 +30,45 @@ class Car {
     }
 
     update(roadBorders) {
-        this.#move();
+        if(!this.damaged) {
+            this.#move();
+            this.polygon = this.#createPolygon();
+            this.damaged = this.#assessDamage(roadBorders);
+        }
         this.sensor.update(roadBorders);
+    }
+
+    #assessDamage(roadBorders) {
+        for( let i = 0; i < roadBorders.length; i++) {
+            if(polysIntersect(this.polygon, roadBorders[i])) {
+                return true;
+            }
+        };
+        return false;
+    }
+
+    #createPolygon() {
+        const points = [];
+        const rad = Math.hypot(this.width, this.height) / 2;
+        const alpha = Math.atan2(this.width, this.height);
+        points.push({
+            x: this.x - Math.sin(this.angle - alpha) * rad,
+            y: this.y - Math.cos(this.angle - alpha) * rad
+        });
+        points.push({
+            x: this.x - Math.sin(this.angle + alpha) * rad,
+            y: this.y - Math.cos(this.angle + alpha) * rad
+        });
+        points.push({
+            x: this.x - Math.sin(Math.PI + this.angle - alpha) * rad,
+            y: this.y - Math.cos(Math.PI + this.angle - alpha) * rad
+        });
+        points.push({
+            x: this.x - Math.sin(Math.PI + this.angle + alpha) * rad,
+            y: this.y - Math.cos(Math.PI + this.angle + alpha) * rad
+        });
+
+        return points;
     }
 
     #move() {
@@ -77,6 +117,9 @@ class Car {
     }
 
     draw(ctx) {
+        if(this.damaged){
+            this.image = this.imageDamage;
+        }
         ctx.save();
         ctx.translate(this.x, this.y);
         ctx.rotate(-this.angle);
